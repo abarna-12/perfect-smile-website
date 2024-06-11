d9bc0eab8fbc6e17f367514a4756e411ad003a5b
abarna-jts <abarnadevi.jorimts@gmail.com>
2024-06-11 13:05:11 +0530

    first commit

---
+++ bds.php
@@ -0,0 +1,324 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Our Services</title>
+    <link rel="stylesheet" href="jobstyle.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"> 
+    <style>
+#form-group .error p{
+    color:#f53b57;
+}
+#form-group .error input{
+    border-color:#f53b57;
+}
+#form-group .success p{
+    color:#115933;
+}
+#form-group .success input{
+    border-color:#05c46b;
+}
+#form-group p{
+    float:left;
+    font-size: 1.9rem;
+    
+}
+#form-group{
+    margin-bottom:10px;
+}
+.error-message {
+color: red;
+font-size:1.5em;
+}
+
+.valid-message {
+color: green;
+font-size:1.5em;
+}
+.success {
+  background-color: #9fd2a1;
+  padding: 5px 10px;
+  color: #326b07;
+  text-align: center;
+  border-radius: 3px;
+  font-size: 14px;
+  margin-top: 10px;
+}
+
+</style>
+</head>
+<body>
+    <!-- NAVBAR HEADER -->
+<header class="header">
+<a href="cr.php">
+<div class="long-arrow-left"></div>
+</a>
+        <a href="#" class="logo">
+        <i class="fas fa-dental"></i> PerfectSmile </a>
+        <nav class="navbar">
+            <ul>
+            <li><a href="./index.php">Home</a></li>
+            <li><a href="./our.services.php">Services</a></li>
+            <li><a href="./booknow.php">Book Online</a></li>
+            <li><a href="./membership.php">Membership plan</a></li>
+            <li><a href="./cr.php">Career</a></li>
+            <li><a href="./aboutus.php">About us</a></li>
+            <li><a href="./logout.php" class="button1">Logout</a></li>   
+        </ul>
+        </nav>    
+            <div id="menu-btn" class="fas fa-bar"></div>    
+    </header>
+<!-- HEADER FOOTER -->
+<section class="home" id="home">
+    <div class="content">
+        <h3>BDS Doctor</h3>
+        <p>Perfect Smile</p>
+         <a href="https://www.facebook.com"> <i class="fa-brands fa-facebook-f"></i>  </a>
+            <a href="https://www.twitter.com"> <i class="fa-brands fa-twitter"></i>  </a>
+            <a href="https://wa.me/7305686253"><i class="fa-brands fa-whatsapp"></i></a>
+            <a href="https://www.instagram.com/"> <i class="fa-brands fa-instagram"></i> </a>
+            <a href="https://www.linkedin.com"> <i class="fa-brands fa-linkedin"></i>  </a> 
+            <a href="https://www.pinterest.com"> <i class="fa-brands fa-pinterest-p"></i>  </a>
+    </div>
+</section>
+
+<!-- JOB DESCRIPTION  -->
+ 
+
+<section class="description" id="description">
+    
+    <button id="showJobDescription" class="tablink" style="margin-left:28rem;">Show Job Description</button>
+    <button id="showAboutCompany" class="tablink">Show About Company</button>
+    <br>
+    <br><br>
+    <br>
+    <br>
+
+        <div class="box1" id="job">
+            
+            
+            <h2>BDS Doctor</h2>
+            <h1>Perfect Smile Clinic</h1>
+            <span class="fa fa-star" style="color:orange;"></span>
+            <span class="fa fa-star" style="color:orange;"></span>
+            <span class="fa fa-star" style="color:orange;"></span>
+            <span class="fa fa-star" style="color:orange;"></span>
+            <span class="fa fa-star"></span>
+            <div class="about">
+            <i class=" fa fa-user"></i>0-2 years  </a>
+            <i class="fa-solid fa-user-plus"></i>2 Openings  </a>
+            <i class="fa fa-inr"></i>Not Disclosed  </a>
+            <i class="fa fa-map-marker"></i>Chennai  </a>
+            </div>
+            <a href="#formid">
+            <button class="btn" id="applyButton">Apply Now </button><br>
+            <a>
+        
+       
+            <br><br>
+           
+            <div class="job-description">
+                <h2>Job Description:</h2>
+                <p>A BDS doctor, or dentist, is responsible for diagnosing and providing dental care to patients. They perform various dental procedures to treat and prevent dental issues, maintain oral health, and improve the appearance of patients' teeth and gums.</p><br>
+                <br>
+                <br>
+            </div>
+            <div class="qualification">
+                <h2>Qualification:</h2>
+                <br>
+                <ul>
+                    <li>Passed 12th board exams with a minimum of 50% aggregate</li>
+                    <li>10, +2 exam or an equivalent degree with at least 85% from a recognized institute.</li>
+                </ul>
+            </div><br><br>
+            <div class="skills">
+                <h2>Skills:</h2>
+                <br>
+                <ul>
+                    <li>Clinical expertise in dental procedures and techniques.</li>
+                    <li>Communication and interpersonal skills to interact with patients effectively.</li>
+                    <li>Proficiency in the use of dental equipment and technology.</li>
+                </ul>  
+            </div>        
+
+</div>
+        </div>
+        <div class="box2" id="about">
+            <h2>About Company</h2>
+            <br>
+            <br>
+            <br>
+            <br>
+            <p class="abdetails">
+            Company Name: Perfect Smile<br>
+            <br>
+            <br>
+            <br>
+
+            We take care of your healthy smile!!
+            At Perfect Smile, we're not just about smiles; we're about creating confidence, one smile at a time. Our mission is to transform dental care into an experience that you actually look forward to. We're a team of passionate professionals dedicated to delivering the best in oral health and aesthetics.
+            <br>
+            <br>
+            </p>
+            <p class="det">
+            <br>
+            <br>
+            
+            <a href="tel:7305686253" style="color:#000000;"> <i class="fas fa-phone"></i> 7305686253 </a><br>
+            <a href="tel:9840734794" style="color:#000000;"> <i class="fas fa-phone"></i> 9840734794 </a><br>
+            <a href="mailto:jdcare1997@gmail.com" style="color:#000000;"> <i class="fas fa-envelope"></i> jdcare1997@gmail.com </a><br>
+            <a href="mailto:perfectsmile1029@gmail.com" style="color:#000000;"> <i class="fas fa-envelope"></i> perfectsmile1029@gmail.com </a><br>
+            <a href="#" style="color:#000000;"> <i class="fas fa-map-marker-alt" style="color:#000000;"></i> Chennai, India - 600075 </a><br>
+</p>
+   
+        </div>
+    </section>
+
+    <section class="form" id="formid" >
+        <br>
+    <h2 class="heading">Application Form</h2><br>
+    <br>
+    <div class="form-details">
+    <form class="myform" method="POST" action="process.php">
+    <h2>Personal Details</h2>
+    <hr class="styled-line">
+    <div>
+    <label for="fname" required >First Name <span class="required">*</span></label><br>
+    <input type="text" id="fname" name="fname"><p id="name-message"></p><br>
+    </div>
+    <div>
+    <label for="lname" >Last Name</label><br>
+    <input type="text" id="lname" name="lname"><p id="lname-message"></p><br>
+    </div>
+    <div>
+    <label for="email">Email <span class="required">*</span></label><br>
+    <input type="email" id="email" name="email"><p id="e-message"><br>
+    </div>
+    <div>
+    <label for="contact">Contact <span class="required">*</span></label><br>
+    <input type="number"  id="contact" name="contact"><p id="contact-message"><br>
+    </div>
+    <div>
+    <label for="gender">Gender <span class="required">*</span></label><br>
+    <input type="text" id="gender" name="gender"><br>
+    </div>
+    <div>
+    <label for="course">Course <span class="required">*</span></label><br>
+    <input type="text"  id="course" name="course"><br>
+    </div>
+    <div>
+    <label for="clgname">College Name <span class="required">*</span></label><br>
+    <input type="text" id="clgname" name="clgname"><br>
+    </div>
+    <div>
+    <label for="year" autocomplete="year">Year of Passing <span class="required">*</span></label><br>
+    <input type="number" id="year" name="year"><br>
+    </div>
+    <div>
+    <label for="location">Current Location <span class="required">*</span></label><br>
+    <input type="text" id="location" name="location"><br>
+    </div>
+    <div id="formMessage" style="color: red; font-size:1.6em;"></div>
+            
+<button class="btn" id="openModal" style="margin-left:25%;">Submit</button><br>
+<a href="cr.php" class="btn" style="float:right; margin-right:43%; margin-top:-5.5%;">Cancel </a>
+</form>
+
+</div>
+
+</section>
+<!-- The Modal -->
+<div id="myModal" class="modal">
+        <div class="modal-content">
+            <span class="close" id="closeModal">&times;</span>
+            <i class="fa-regular fa-circle-check"></i>
+            <h2>Applied Successfully</h2>
+            <p>Thank You for your application. We will required you later. Check your Mail.
+            </p>
+        </div>
+</div>
+<section class="related-job">
+<h3>Related Jobs</h3>
+<div class="job-list">
+    
+    <br>
+
+    <div class="table1">
+      <h2 class="job-title">Front office Executive</h2>
+      <img src="image/front.png">
+      <p class="details">Responsible for collecting, analyzing and interpreting large data sets to help organizations make better decisions.</p>
+      <a href="front.php" class="btn">Apply Now </a>
+    </div>
+    <div class="table2" style="width: 70%;">
+    <h2 class="job-title">Dental Surgeon</h2>
+      <img src="image/surg.png">
+      <p class="details">Responsible for planning, executing and closing projects on time and within budget.</p>
+      <a href="surgeon.php" class="btnb">Apply Now </a>
+    </div>
+    <div class="table3">
+    <h2 class="job-title">General Dentist</h2>
+      <img src="image/dental.png">
+      <p class="details">Responsible for managing the entire product life cycle, from ideation to launch and post-launch maintenance.</p>
+      <a href="gdentist.php" class="btn">Apply Now </a>
+    </div>
+    <div class="table4" style="width: 70%;">
+    <h2 class="job-title">Dental Nurse</h2>
+      <img src="image/nurse.png">
+      <p class="details">Responsible for reaching out to potential customers and closing sales deals.</p>
+      <a href="nurse.php" class="btnb">Apply Now </a>
+    </div>
+</div>
+
+</section>
+
+<section class="footer">
+    <div class="box-container">
+        <div class="box">
+            <h3>Quick Links</h3>
+            <a href="./index.php"> <i class="fas fa-chevron-right"></i> Home </a>
+            <a href="./aboutus.php"> <i class="fas fa-chevron-right"></i> About us </a>
+            <a href="./our.services.php"> <i class="fas fa-chevron-right"></i> Services </a>
+            <a href="./booknow.php"> <i class="fas fa-chevron-right"></i> Appointments </a>
+            <a href="./membership.php"> <i class="fas fa-chevron-right"></i> Membership plan </a>
+            <a href="./cr.php"> <i class="fas fa-chevron-right"></i> Career </a>
+            <a href="./login.php"> <i class="fas fa-chevron-right"></i> Login/Sign Up </a>          
+        </div>
+
+        <div class="box">
+            <h3>Our Services</h3>
+            <a href="./services1.php"> <i class="fas fa-chevron-right"></i> Braces Orthodontics </a>
+            <a href="./services2.php"> <i class="fas fa-chevron-right"></i> Kids Dentistry </a>
+            <a href="./services3.php"> <i class="fas fa-chevron-right"></i> Root Canal Therapy </a>
+            <a href="./services4.php"> <i class="fas fa-chevron-right"></i> Dental Implants </a>
+            <a href="./services5.php"> <i class="fas fa-chevron-right"></i> Gum Therapy </a>
+            <a href="./services6.php"> <i class="fas fa-chevron-right"></i> Teeth Whitening </a>
+            <a href="./services7.php"> <i class="fas fa-chevron-right"></i> Laser Dentistry </a> 
+            <a href="./services8.php"> <i class="fas fa-chevron-right"></i> Teeth Removal </a>                       
+        </div>
+        
+        <div class="box">
+            <h3>Our Services</h3>
+            <a href="tel:7305686253"> <i class="fas fa-phone"></i> 7305686253 </a>
+            <a href="tel:9840734794"> <i class="fas fa-phone"></i> 9840734794 </a>
+            <a href="mailto:jdcare1997@gmail.com"> <i class="fas fa-envelope"></i> jdcare1997@gmail.com </a>
+            <a href="mailto:perfectsmile1029@gmail.com"> <i class="fas fa-envelope"></i> perfectsmile1029@gmail.com </a> 
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> Chennai, India - 600075 </a>                               
+        </div>
+
+        <div class="box">
+            <h3>Follow Us</h3>
+            <a href="https://www.facebook.com"> <i class="fa-brands fa-facebook-f"></i> Facebook </a>
+            <a href="https://www.twitter.com"> <i class="fa-brands fa-twitter"></i> Twitter </a>
+            <a href="https://www.instagram.com/"> <i class="fa-brands fa-instagram"></i> Instagram </a>
+            <a href="https://www.linkedin.com"> <i class="fa-brands fa-linkedin"></i> LinkedIn </a> 
+            <a href="https://www.pinterest.com"> <i class="fa-brands fa-pinterest-p"></i> Pinterest </a>                               
+        </div>
+    </div>
+
+    <div class="credit"> Created by <a href="https://jorimts.com/"> Jorim Technology Solutions Pvt Ltd</a> | all rights reserved </div>
+</section>
+
+<script src="crvalid.js"></script>
+</body>
+</html>
---
+++ contactus.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact us</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <link rel="stylesheet" href="contactstyle.css">
+    
+</head>
+<body>
+<!--footer section starts-->
+<header class="header">
+        <a href="#" class="logo">
+           
+        <i class="fas fa-dental"></i> PerfectSmile </a>
+    
+        <nav class="navbar">
+            <ul>
+            <li><a href="./index.php">Home</a></li>
+            <li><a href="./our.services.php">Services</a></li>
+            <li><a href="./booknow.php">Book Online</a></li>
+            <li><a href="./membership.php">Membership plan</a></li>
+            <li><a href="./cr.php">Career</a></li>
+            <li><a href="./aboutus.php">About us</a></li>
+            <li><a href="./login.php" class="button">Login</a></li>
+            <!-- <a href="./Account.php" class="scroll">Account -->
+                <!-- <p> Welcome
+                    <div class="menu">
+                    <?php echo $_SESSION['email'];?>
+                    </div> -->
+                <!-- </p> -->
+            <!-- </a> -->
+            <li><a href="./logout.php" class="button1">Logout</a></li>   
+        </ul>
+        </nav>    
+            <div id="menu-btn" class="fas fa-bar"></div>    
+    </header>
+
+    <section class="home" id="home">
+    <div class="image">
+    <img src="image/pl.png">
+    </div>
+    <div class="content">
+        <h3>Perfect Smile</h3>
+        <p>Where Smiles Shine Brighter Every Day!</p>
+        <a href="contactus.php" class="btn"> contact us </a>
+    </div>
+</section>
+<section class="footer">
+    <div class="box-container">
+        <div class="box">
+            <h3>Quick Links</h3>
+            <a href="./index.php"> <i class="fas fa-chevron-right"></i> Home </a>
+            <a href="./aboutus.php"> <i class="fas fa-chevron-right"></i> About us </a>
+            <a href="./our.services.php"> <i class="fas fa-chevron-right"></i> Services </a>
+            <a href="./booknow.php"> <i class="fas fa-chevron-right"></i> Appointments </a>
+            <a href="./membership.php"> <i class="fas fa-chevron-right"></i> Membership plan </a>
+            <a href="./cr.php"> <i class="fas fa-chevron-right"></i> Career </a>
+            <a href="./login.php"> <i class="fas fa-chevron-right"></i> Login/Sign Up </a>          
+        </div>
+
+        <div class="box">
+            <h3>Our Services</h3>
+            <a href="./services1.php"> <i class="fas fa-chevron-right"></i> Braces Orthodontics </a>
+            <a href="./services2.php"> <i class="fas fa-chevron-right"></i> Kids Dentistry </a>
+            <a href="./services3.php"> <i class="fas fa-chevron-right"></i> Root Canal Therapy </a>
+            <a href="./services4.php"> <i class="fas fa-chevron-right"></i> Dental Implants </a>
+            <a href="./services5.php"> <i class="fas fa-chevron-right"></i> Gum Therapy </a>
+            <a href="./services6.php"> <i class="fas fa-chevron-right"></i> Teeth Whitening </a>
+            <a href="./services7.php"> <i class="fas fa-chevron-right"></i> Laser Dentistry </a> 
+            <a href="./services8.php"> <i class="fas fa-chevron-right"></i> Teeth Removal </a>                       
+        </div>
+        
+        <div class="box">
+            <h3>Our Services</h3>
+            <a href="tel:7305686253"> <i class="fas fa-phone"></i> 7305686253 </a>
+            <a href="tel:9840734794"> <i class="fas fa-phone"></i> 9840734794 </a>
+            <a href="mailto:jdcare1997@gmail.com"> <i class="fas fa-envelope"></i> jdcare1997@gmail.com </a>
+            <a href="mailto:perfectsmile1029@gmail.com"> <i class="fas fa-envelope"></i> perfectsmile1029@gmail.com </a> 
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> Chennai, India - 600075 </a>                               
+        </div>
+
+        <div class="box">
+            <h3>Follow Us</h3>
+            <a href="https://www.facebook.com"> <i class="fa-brands fa-facebook-f"></i> Facebook </a>
+            <a href="https://www.twitter.com"> <i class="fa-brands fa-twitter"></i> Twitter </a>
+            <a href="https://www.instagram.com/"> <i class="fa-brands fa-instagram"></i> Instagram </a>
+            <a href="https://www.linkedin.com"> <i class="fa-brands fa-linkedin"></i> LinkedIn </a> 
+            <a href="https://www.pinterest.com"> <i class="fa-brands fa-pinterest-p"></i> Pinterest </a>                               
+        </div>
+    </div>
+
+    <div class="credit"> Created by <a href="https://jorimts.com/"> Jorim Technology Solutions Pvt Ltd</a> | all rights reserved </div>
+</section>
+
+<!--footer section ends-->
+
+    
+
+
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
